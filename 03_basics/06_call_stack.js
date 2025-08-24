677b66328658b5e7e15fd7bf44317ccfcce891c1
Sayan520 <ghoshsayan5205@gmail.com>
2025-08-24 13:18:54 +0530

    JEC draw.io added

--- 03_basics/06_call_stack.js
+++ 03_basics/06_call_stack.js
@@ -1,5 +1,20 @@
-/* try in browser >> Sources >> Snippets >> test.js */
+// Javascript executional context
 
+let val1 = 10
+let val2 = 5
+
+function addNum(num1, num2) {
+	let result = num1 + num2
+	return result
+}
+
+const result1 = addNum(val1, val2)
+const result2 = addNum(10, 19)
+
+console.log(result1); 
+console.log(result2); 
+
+/* Call Stack: try in browser >> Sources >> Snippets >> test.js */
 function one() {
   console.log("one");
   two()
